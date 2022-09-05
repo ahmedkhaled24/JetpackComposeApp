38fbd4074068b7ea6e3c9bac7c1cbc7c9f57bfa7
Ahmed Khaled <ahmedhamtary@gmail.com>
2022-09-05 12:58:02 +0200

    complete 6

--- app/src/main/java/com/momentum/learnjetpack/MainActivity.kt
+++ app/src/main/java/com/momentum/learnjetpack/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.momentum.learnjetpack
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -31,53 +24,56 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.momentum.learnjetpack.ui.theme.LearnJetpackTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Gray)) {
-                Text(text = buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)){
-                        append("A")
-                    }
-
-                    append("hme")
-
-                    withStyle(style = SpanStyle(color = Color.Blue, fontSize = 50.sp)){
-                        append("d")
-                    }
-                }, color = Color.White, fontSize = 40.sp)
-            }
-
-
-//            Box(modifier = Modifier
-//                .fillMaxSize()
-//                .background(Color(0xFF101010))) {
-//
-//                Text(text = buildAnnotatedString() {
-//                    withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)){
-//                        append("J")
-//                    }
-//
-//                    append("etpack ")
-//
-//                    withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)){
-//                        append("C")
-//                    }
-//
-//                    append("ompose")
-//                },
-//                color = Color.White,
-//                fontSize = 30.sp,
-//                textDecoration = TextDecoration.Underline)
+            //--lesson 6 -- part 1 --
+//            Column(Modifier.fillMaxSize()) {
+//                val color = remember { mutableStateOf(Color.Red) }
+//                ColorBox(modifier = Modifier.weight(1f).fillMaxSize())
+//                Box(modifier = Modifier.background(color.value).weight(1f).fillMaxSize())
 //            }
+            //--lesson 6 -- part 1 --
+
+
+            //--lesson 6 -- part 2 --
+            Column(Modifier.fillMaxSize()) {
+                val color = remember { mutableStateOf(Color.Yellow) }
+                ColorBox2(modifier = Modifier.weight(1f).fillMaxSize()){
+                    color.value = it
+                }
+                Box(modifier = Modifier.background(color.value).weight(1f).fillMaxSize())
+            }
+            //--lesson 6 -- part 2 --
+
+
         }
     }
 }
+
+//--lesson 6 -- part 1 --
+@Composable
+fun ColorBox(modifier: Modifier = Modifier){
+    val color = remember { mutableStateOf(Color.Yellow) }
+    Box(modifier = modifier.background(color.value).clickable {
+        color.value = Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+    })
+}
+//--lesson 6 -- part 1 --
+
+
+//--lesson 6 -- part 2 --
+@Composable
+fun ColorBox2(modifier: Modifier = Modifier, updateColor : (Color) -> Unit){
+    Box(modifier = modifier.background(Color.Red).clickable {
+        updateColor(Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f))
+    })
+}
+//--lesson 6 -- part 2 --
 
 
 
