353b212e07c49fda478485112d67a6abfe09aaea
Ahmed Khaled <ahmedhamtary@gmail.com>
2022-09-06 12:16:15 +0200

    complete lesson 12

--- app/src/main/java/com/momentum/learnjetpack/MainActivity.kt
+++ app/src/main/java/com/momentum/learnjetpack/MainActivity.kt
@@ -3,44 +3,32 @@ package com.momentum.learnjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Easing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                tween(durationMillis = 2000,
-                delayMillis = 300,
-                easing = LinearOutSlowInEasing)
-            )
-            Box(
-                modifier = Modifier.size(size).background(Color.Red),
-                contentAlignment = Alignment.Center){
-                
-                Button(onClick = {
-                    sizeState+= 50.dp
-                }) {
-                    Text(text = "Increase Size")
-                }
+
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                CircularProgressBar(percentage = 0.8f, number = 100)
             }
 
         }
@@ -48,22 +36,53 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+){
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
 
-//var sizeState by remember { mutableStateOf(200.dp) }
-//val size by animateDpAsState(
-//    targetValue = sizeState,
-//    tween(durationMillis = 2000,
-//        delayMillis = 300,
-//        easing = LinearOutSlowInEasing)
-//)
-//Box(
-//modifier = Modifier.size(size).background(Color.Red),
-//contentAlignment = Alignment.Center){
-//
-//    Button(onClick = {
-//        sizeState+= 50.dp
-//    }) {
-//        Text(text = "Increase Size")
-//    }
-//}
+    val curPercentage = animateFloatAsState(
+        targetValue =if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
 
+    LaunchedEffect(key1 = true){
+        animationPlayed = true
+    }
+
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        Text(
+            text =(curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
